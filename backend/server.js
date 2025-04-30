4e61410b6a3b9611682e5619204911847627e325
siddheshdhumatkar423 <siddheshdhumatkar423@gmail.com>
2025-04-30 11:28:22 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 5000;
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello from backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
